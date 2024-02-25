bcf3831a3a3d265f1ed18d8753c3ae14f351048c
jiexiao <jiexiao@cs.washington.edu>
2024-02-25 12:42:54 -0800

    integrating dual number to the program, still have bugs

--- diffraytracer.cpp
+++ diffraytracer.cpp
@@ -14,7 +14,7 @@
 #include <Eigen/Dense>
 
 // replace the vec3
-using namespace autodiff;
+using autodiff::real;
 using vec3 = Eigen::Matrix<real, 3, 1>;
 
 // TODO: Add Loss Function 
@@ -39,14 +39,14 @@ const Material      glass = {1.5, {0.0,  0.9, 0.1, 0.8}, {0.6, 0.7, 0.8},  125.}
 const Material red_rubber = {1.0, {1.4,  0.3, 0.0, 0.0}, {0.3, 0.1, 0.1},   10.};
 const Material     mirror = {1.0, {0.0, 16.0, 0.8, 0.0}, {1.0, 1.0, 1.0}, 1425.};
 
-constexpr Sphere spheres[] = {
+const Sphere spheres[] = {
     {{-3,    0,   -16}, 2,      ivory},
     {{-1.0, -1.5, -12}, 2,      glass},
     {{ 1.5, -0.5, -18}, 3, red_rubber},
     {{ 7,    5,   -18}, 4,     mirror}
 };
 
-constexpr vec3 lights[] = {
+const vec3 lights[] = {
     {-20, 20,  20},
     { 30, 50, -25},
     { 30, 20,  30}
@@ -57,20 +57,20 @@ vec3 reflect(const vec3 &I, const vec3 &N) {
 }
 
 vec3 refract(const vec3 &I, const vec3 &N, const float eta_t, const float eta_i=1.f) { // Snell's law
-    float cosi = - std::max(-1.f, std::min(1.f, I*N));
+    real cosi = - max(real(-1), min(real(1), I.dot(N)));
     if (cosi<0) return refract(I, -N, eta_i, eta_t); // if the ray comes from the inside the object, swap the air and the media
-    float eta = eta_i / eta_t;
-    float k = 1 - eta*eta*(1 - cosi*cosi);
-    return k<0 ? vec3{1,0,0} : I*eta + N*(eta*cosi - std::sqrt(k)); // k<0 = total reflection, no ray to refract. I refract it anyways, this has no physical meaning
+    real eta = eta_i / eta_t;
+    real k = 1 - eta*eta*(1 - cosi*cosi);
+    return k<0 ? vec3{1,0,0} : I*eta + N*(eta*cosi - sqrt(k)); // k<0 = total reflection, no ray to refract. I refract it anyways, this has no physical meaning
 }
 
-std::tuple<bool,float> ray_sphere_intersect(const vec3 &orig, const vec3 &dir, const Sphere &s) { // ret value is a pair [intersection found, distance]
+std::tuple<bool,real> ray_sphere_intersect(const vec3 &orig, const vec3 &dir, const Sphere &s) { // ret value is a pair [intersection found, distance]
     vec3 L = s.center - orig;
-    float tca = L*dir;
-    float d2 = L*L - tca*tca;
+    real tca = L.dot(dir);
+    real d2 = L.dot(L) - tca*tca;
     if (d2 > s.radius*s.radius) return {false, 0};
-    float thc = std::sqrt(s.radius*s.radius - d2);
-    float t0 = tca-thc, t1 = tca+thc;
+    real thc = sqrt(s.radius*s.radius - d2);
+    real t0 = tca-thc, t1 = tca+thc;
     if (t0>.001) return {true, t0};  // offset the original point by .001 to avoid occlusion by the object itself
     if (t1>.001) return {true, t1};
     return {false, 0};
@@ -80,15 +80,15 @@ std::tuple<bool,vec3,vec3,Material> scene_intersect(const vec3 &orig, const vec3
     vec3 pt, N;
     Material material;
 
-    float nearest_dist = 1e10;
-    if (std::abs(dir.y)>.001) { // intersect the ray with the checkerboard, avoid division by zero
-        float d = -(orig.y+4)/dir.y; // the checkerboard plane has equation y = -4
+    real nearest_dist = 1e10;
+    if (abs(dir.y())>.001) { // intersect the ray with the checkerboard, avoid division by zero
+        real d = -(orig.y() + real(4)) / dir.y(); // the checkerboard plane has equation y = -4
         vec3 p = orig + dir*d;
-        if (d>.001 && d<nearest_dist && std::abs(p.x)<10 && p.z<-10 && p.z>-30) {
+        if (d>real(.001) && d<nearest_dist && abs(p.x())<real(10) && p.z()<real(-10) && p.z()>real(-30)) {
             nearest_dist = d;
             pt = p;
             N = {0,1,0};
-            material.diffuse_color = (int(.5*pt.x+1000) + int(.5*pt.z)) & 1 ? vec3{.3, .3, .3} : vec3{.3, .2, .1};
+            material.diffuse_color = (int(.5*pt.x()+1000) + int(.5*pt.z())) & 1 ? vec3{.3, .3, .3} : vec3{.3, .2, .1};
         }
     }
 
@@ -115,18 +115,18 @@ vec3 cast_ray(const vec3 &orig, const vec3 &dir, const int depth=0) {
     vec3 reflect_color = cast_ray(point, reflect_dir, depth + 1);
     vec3 refract_color = cast_ray(point, refract_dir, depth + 1);
 
-    float diffuse_light_intensity = 0, specular_light_intensity = 0;
+    real diffuse_light_intensity = 0, specular_light_intensity = 0;
     for (const vec3 &light : lights) { // checking if the point lies in the shadow of the light
         vec3 light_dir = (light - point).normalized();
         auto [hit, shadow_pt, trashnrm, trashmat] = scene_intersect(point, light_dir);
         if (hit && (shadow_pt-point).norm() < (light-point).norm()) continue;
-        diffuse_light_intensity  += std::max(0.f, light_dir*N);
-        specular_light_intensity += std::pow(std::max(0.f, -reflect(-light_dir, N)*dir), material.specular_exponent);
+        diffuse_light_intensity  += max(real(0), light_dir.dot(N));
+        specular_light_intensity += pow(max(real(0), -reflect(-light_dir, N).dot(dir)), material.specular_exponent);
     }
     return material.diffuse_color * diffuse_light_intensity * material.albedo[0] + vec3{1., 1., 1.}*specular_light_intensity * material.albedo[1] + reflect_color*material.albedo[2] + refract_color*material.albedo[3];
 }
 
-float loss(std::vector<unsigned char> imageX, std::vector<unsigned char> imagey) {
+real loss(std::vector<unsigned char> imageX, std::vector<unsigned char> imagey) {
     return 0.0f;
 }
 
@@ -147,8 +147,8 @@ int main() {
 
     for (int i = 0; i < width * height; ++i) {
         for (int chan = 0; chan < 3; ++chan) {
-            float max = std::max(1.f, std::max(framebuffer[i][0], std::max(framebuffer[i][1], framebuffer[i][2])));
-            image[i * 3 + chan] = static_cast<unsigned char>(255 * std::max(0.f, std::min(1.f, framebuffer[i][chan] / max)));
+            real max_val = max(real(1), max(framebuffer[i][0], max(framebuffer[i][1], framebuffer[i][2])));
+            image[i * 3 + chan] = static_cast<unsigned char>(255 * max(real(0), min(real(1), framebuffer[i][chan] / max_val)));
         }
     }
 
