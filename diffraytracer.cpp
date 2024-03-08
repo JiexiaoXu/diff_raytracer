30aef6fc51d50986148c420a00addb0eab7e99ca
jiexiao <jiexiao@cs.washington.edu>
2024-03-07 19:58:14 -0800

    clean some comment and fix typo

--- diffraytracer.cpp
+++ diffraytracer.cpp
@@ -43,11 +43,6 @@ struct Material {
 //     Material material;
 // };
 
-// struct parameters {
-//     Sphere spehres[];
-//     vec3 lights[];
-// };
-
 struct Triangle {
     vec3 p1;
     vec3 p2;
@@ -59,13 +54,6 @@ const Material      ivory = {1.0, {0.9,  0.5, 0.1, 0.0}, {0.4, 0.4, 0.3},   50.}
 // const Material      glass = {1.5, {0.0,  0.9, 0.1, 0.8}, {0.6, 0.7, 0.8},  125.};
 // const Material red_rubber = {1.0, {1.4,  0.3, 0.0, 0.0}, {0.3, 0.1, 0.1},   10.};
 // const Material     mirror = {1.0, {0.0, 16.0, 0.8, 0.0}, {1.0, 1.0, 1.0}, 1425.};
-
-// Sphere spheres[] = {
-//     {{-3,    0,   -16}, 2,      ivory}
-//     {{-1.0, -1.5, -12}, 2,      glass},
-//     {{ 1.5, -0.5, -18}, 3, red_rubber},
-//     {{ 7,    5,   -18}, 4,     mirror}
-// };
 
 Triangle triangles[] = {
     {{-3, 0, -16}, {-1.0, -1.5, -12},{1.5, -0.5, -18}, ivory}
@@ -96,26 +84,6 @@ vec3 refract(const vec3 &I, const vec3 &N, const var eta_t, const var eta_i=1.f)
     var k = 1 - eta*eta*(1 - cosi*cosi);
     return k<0 ? vec3{1,0,0} : I*eta + N*(eta*cosi - sqrt(k)); // k<0 = total reflection, no ray to refract. I refract it anyways, this has no physical meaning
 }
-
-// vec3const refract(const vec3const &I, const vec3const &N, const double eta_t, const double eta_i=1.f) { // Snell's law
-//     double cosi = - std::max(-1.0, std::min(1.0, I.dot(N)));
-//     if (cosi<0) return refract(I, -N, eta_i, eta_t); // if the ray comes from the inside the object, swap the air and the media
-//     double eta = eta_i / eta_t;
-//     double k = 1 - eta*eta*(1 - cosi*cosi);
-//     return k<0 ? vec3const{1,0,0} : I*eta + N*(eta*cosi - std::sqrt(k)); // k<0 = total reflection, no ray to refract. I refract it anyways, this has no physical meaning
-// }
-
-// std::tuple<bool,var> ray_sphere_intersect(const vec3 &orig, const vec3 &dir, const Sphere &s) { // ret value is a pair [intersection found, distance]
-//     vec3 L = s.center - orig;
-//     var tca = L.dot(dir);
-//     var d2 = L.dot(L) - tca*tca;
-//     if (d2 > s.radius*s.radius) return {false, 0};
-//     var thc = sqrt(s.radius*s.radius - d2);
-//     var t0 = tca-thc, t1 = tca+thc;
-//     if (t0>.001) return {true, t0};  // offset the original point by .001 to avoid occlusion by the object itself
-//     if (t1>.001) return {true, t1};
-//     return {false, 0};
-// }
 
 std::tuple<bool, var, vec3> ray_triangle_intersect(const vec3 &orig, const vec3 &dir, const Triangle &t) {
     var u, v;  // barycentric coordinates, last one is (1 - u - v)
@@ -157,24 +125,15 @@ std::tuple<bool,vec3,vec3,Material> scene_intersect(const vec3 &orig, const vec3
     Material material;
 
     var nearest_dist = 1e10;
-    if (abs(dir.y())>.001) { // intersect the ray with the checkerboard, avoid division by zero
-        var d = -(orig.y() + 4) / dir.y(); // the checkerboard plane has equation y = -4
-        vec3 p = orig + dir * d;
-        if (d>.001 && d<nearest_dist && abs(p.x())<10 && p.z()<-10 && p.z()>-30) {
-            nearest_dist = d;
-            pt = p;
-            N = {0,1,0};
-            material.diffuse_color = (int(val(.5 * pt.x() + 1000)) + int(val(.5 * pt.z()))) & 1 ? vec3const{.3, .3, .3} : vec3const{.3, .2, .1};
-        }
-    }
-
-    // for (const Sphere &s : spheres) { // intersect the ray with all spheres
-    //     auto [intersection, d] = ray_sphere_intersect(orig, dir, s);
-    //     if (!intersection || d > nearest_dist) continue;
-    //     nearest_dist = val(d);
-    //     pt = orig + dir*nearest_dist;
-    //     N = (pt - s.center).normalized();
-    //     material = s.material;
+    // if (abs(dir.y())>.001) { // intersect the ray with the checkerboard, avoid division by zero
+    //     var d = -(orig.y() + 4) / dir.y(); // the checkerboard plane has equation y = -4
+    //     vec3 p = orig + dir * d;
+    //     if (d>.001 && d<nearest_dist && abs(p.x())<10 && p.z()<-10 && p.z()>-30) {
+    //         nearest_dist = d;
+    //         pt = p;
+    //         N = {0,1,0};
+    //         material.diffuse_color = (int(val(.5 * pt.x() + 1000)) + int(val(.5 * pt.z()))) & 1 ? vec3const{.3, .3, .3} : vec3const{.3, .2, .1};
+    //     }
     // }
 
     for (const Triangle &t : triangles) {
@@ -249,7 +208,7 @@ void backpropagation(Triangle &t, const std::vector<vec3> &img_X, const std::vec
     // Update parameters
     t.p1 -= learning_rate * dp1;
     t.p2 -= learning_rate * dp2;
-    t.p2 -= learning_rate * dp3;
+    t.p3 -= learning_rate * dp3;
 }
 
 
@@ -269,7 +228,7 @@ void train(std::vector<vec3>& framebuffer, const std::vector<vec3> target_fb, in
         }
     }
 
-    float learningRate = 0.01; // Setting this to 0.01, but change based on speed and accuracy
+    float learningRate = 1.0; // Setting this to 0.01, but change based on speed and accuracy
     backpropagation(triangles[0], framebuffer, target_fb, learningRate, width, height); // spheres[0] is the sphere we're trying to render
 }
 
